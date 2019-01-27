f4858803a95a0b0d724908569973c46f474548e1
Deliman206 <smiller206@me.com>
2019-01-27 11:43:22 -0800

    cleanedup file tree

--- Lab07LINQ/Lab07LINQ/Program.cs
+++ Lab07LINQ/Lab07LINQ/Program.cs
@@ -11,10 +11,9 @@ namespace Lab07LINQ
     public class Program
     {
         static void Main(string[] args)
-        { 
+        {
             // Convert JSON to string
             string jsonFile = ReadFile("../../../data.json");
-
             // Create Objects from JSON string to create Collection
             RootObject locations = JsonConvert.DeserializeObject<RootObject>(jsonFile);
 
@@ -25,7 +24,7 @@ namespace Lab07LINQ
             {
                 Console.Write($"{location},\n");
             }
-            Console.WriteLine("\n\n");
+            Console.WriteLine("========================================");
 
             Console.WriteLine("Filter out all the neighborhoods that do not have any names");
             IEnumerable<string> allNamesNoBlanks = AllNeighborhoodsWithNames(allNames);
@@ -33,7 +32,7 @@ namespace Lab07LINQ
             {
                 Console.Write($"{location},\n");
             }
-            Console.WriteLine("\n\n");
+            Console.WriteLine("========================================");
 
             Console.WriteLine("Remove the Duplicates");
             IEnumerable<string> allNamesNoDupes = allNamesNoBlanks.Distinct();
@@ -41,15 +40,15 @@ namespace Lab07LINQ
             {
                 Console.Write($"{location},\n");
             }
-            Console.WriteLine("\n\n");
+            Console.WriteLine("========================================");
 
             Console.WriteLine("Rewrite the queries from above, and consolidate all into one single query.");
-            IEnumerable<string> neighborhoodsNoDupes = allNames.Where(n => n != "").Distinct();
+            IEnumerable<string> neighborhoodsNoDupes = locations.features.Select(x => x.properties.neighborhood).Where(n => n != "").Distinct();
             foreach (string location in neighborhoodsNoDupes)
             {
                 Console.Write($"{location},\n");
             }
-            Console.WriteLine("\n\n");
+            Console.WriteLine("========================================");
 
             Console.WriteLine("Rewrite at least one of these questions only using the opposing method(example: Use LINQ instead of a Lambda and vice versa.");
             IEnumerable<string> AllNeighborhoodsWithNamesAGAIN = allNames.Where(n => n != "");
@@ -58,7 +57,7 @@ namespace Lab07LINQ
                 Console.Write($"{location},\n");
             }
             Console.WriteLine("\n\n");
-            Console.Read();  
+            Console.Read();
         }
         /// <summary>
         /// Method to read our JSON file and convert to StreamReader
@@ -68,7 +67,7 @@ namespace Lab07LINQ
         public static string ReadFile(string path)
         {
             string x;
-            using(StreamReader sr = new StreamReader(path))
+            using (StreamReader sr = new StreamReader(path))
             {
                 x = sr.ReadToEnd();
             }
@@ -84,15 +83,14 @@ namespace Lab07LINQ
             var queryAllNeighborhoods =
                 from location in locations.features
                 select location.properties.neighborhood;
-
-            return queryAllNeighborhoods;
+           return queryAllNeighborhoods;
         }
         /// <summary>
         /// Method to query the JSON for all neighbors except those with neighborhood = ""
         /// </summary>
         /// <param name="locations">Enumerable List</param>
         /// <returns>Enumerable List</returns>
-        public static RootObject AllNeighborhoodsWithNames(IEnumerable<string> locations)
+        public static IEnumerable<string> AllNeighborhoodsWithNames(IEnumerable<string> locations)
         {
             var queryAllNeighborhoods =
                 from location in locations
@@ -101,6 +99,26 @@ namespace Lab07LINQ
 
             return queryAllNeighborhoods;
         }
-        
+        /// <summary>
+        /// Method to create a new StreamWriter
+        /// </summary>
+        /// <param name="path"></param>
+        public static void CreateFile(string path)
+        {
+            StreamWriter streamWriter = new StreamWriter(path);
+
+        }
+        /// <summary>
+        /// Method to update a StreamWriter
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="word"></param>
+        public static void UpdateFile(string path, string word)
+        {
+            using (StreamWriter streamWriter = File.AppendText(path))
+            {
+                streamWriter.WriteLine(word);
+            }
+        }
     }
 }
